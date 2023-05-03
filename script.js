cb53bec9c1ffa6edde1dec37964159335c34e447
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-05-03 14:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ const apiUrls = [
 ];
 
 // You can write your code here
+// Function to fetch data using Promise.all
+function fetchDataWithPromiseAll() {
+  const startTime = performance.now();
+
+  Promise.all(apiUrls.map(url => fetch(url)))
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = endTime - startTime;
+      document.getElementById("output-all").innerHTML = timeTaken.toFixed(2);
+    })
+    .catch(error => console.log(error));
+}
+
+// Function to fetch data using Promise.any
+function fetchDataWithPromiseAny() {
+  const startTime = performance.now();
+
+  Promise.any(apiUrls.map(url => fetch(url).then(response => response.json())))
+    .then(data => {
+      const endTime = performance.now();
+      const timeTaken = endTime - startTime;
+      document.getElementById("output-any").innerHTML = timeTaken.toFixed(2);
+    })
+    .catch(error => console.log(error));
+}
+
+fetchDataWithPromiseAll();
+fetchDataWithPromiseAny()
